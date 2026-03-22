06e889a6e542474d0773d244ca61b561435e5e21
Cristian <carincon93@gmail.com>
2026-03-22 10:30:44 -0500

    fix: Invoice data

--- src/stores/invoicesStore.ts
+++ src/stores/invoicesStore.ts
@@ -1,11 +1,12 @@
 import { atom } from 'nanostores'
+import { type Invoice, invoices } from '@/lib/data'
 
-export const invoicesStore = atom<string[]>([])
+export const invoicesStore = atom<Invoice[]>([])
 
-export function toggleInvoice(invoice: string) {
+export function toggleInvoice(invoice: Invoice) {
     const currentInvoices = invoicesStore.get()
-    if (currentInvoices.includes(invoice)) {
-        invoicesStore.set(currentInvoices.filter((i) => i !== invoice))
+    if (currentInvoices.some((i) => i.invoice === invoice.invoice)) {
+        invoicesStore.set(currentInvoices.filter((i) => i.invoice !== invoice.invoice))
     } else {
         invoicesStore.set([...currentInvoices, invoice])
     }
